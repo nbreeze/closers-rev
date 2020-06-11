9b403c1fd724e2da3141907c3350e7b4c69bf007
Nikolai Bandong <nikolaibandong@outlook.com>
2020-06-10 18:20:38 -0700

    Typo

--- CMFArchive.h
+++ CMFArchive.h
@@ -1,13 +1,16 @@
 
-class CMFArchive
+namespace CW
 {
-public:
-    struct FileEntry
+    class CMFArchive
     {
-        wchar_t[256] fileName;          // 512
-        unsigned int size_uncompressed; // 516
-        unsigned int size_compressed;   // 520
-        unsigned int data_offset;       // 524
-        unsigned int compressed;        // 528
+    public:
+        struct FileEntry
+        {
+            wchar_t fileName[256];          // 512
+            unsigned int size_uncompressed; // 516
+            unsigned int size_compressed;   // 520
+            unsigned int data_offset;       // 524
+            unsigned int compressed;        // 528
+        };
     };
-}
+} // namespace CW
--- NTF.h
+++ NTF.h
@@ -1,7 +1,11 @@
-enum
+
+namespace CW
 {
-    NTF_DATA_TYPE_CHAR = 2,
-    NTF_DATA_TYPE_INT = 5,
-    NTF_DATA_TYPE_UNICODE_STRING = 8,
-    NTF_DATA_TYPE_FLOAT = 9
+    enum
+    {
+        NTF_DATA_TYPE_CHAR = 2,
+        NTF_DATA_TYPE_INT = 5,
+        NTF_DATA_TYPE_UNICODE_STRING = 8,
+        NTF_DATA_TYPE_FLOAT = 9
+    };
 }
